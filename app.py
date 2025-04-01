20e326242ef00de8efd2e0a16fb4f49d7ef30805
Flenker2006 <flenkermelody93@gmail.com>
2025-03-31 19:00:47 -0500

    Syntax fixes in app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 
-def show-time-date():
+def showTimeDate():
     try:
-        result = supprocess.run(['date'],. stdout=subprocess.PIPE, text=True)
-        current-time-date = result.stdout.strip()
-        return f"<h1>{current-time-date}</h1><p> is the current date and time.</p>"
+        result = supbrocess.run(['date'], stdout=subprocess.PIPE, text=True)
+        currentTimeDate = result.stdout.strip()
+        return f"<h1>{currentTimeDate}</h1><p> is the current date and time.</p>"
     except Exception as Err:
         return f"<p>ErrorL {str(Err)}</p>"
 
